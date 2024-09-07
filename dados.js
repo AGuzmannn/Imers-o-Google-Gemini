bf888bdbe1b81df925a7e8acf1d288d2ceced686
Alexandre Guzmán <102556942+AGuzmannn@users.noreply.github.com>
2024-09-07 19:12:41 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Harry Potter e a Pedra Filosofal",
+        descricao: "Harry Potter e a Pedra Filosofal: 1 Capa dura – 19 agosto 2017, Edição Português  por J.K. Rowling (Autor), Lia Wyler (Tradutor), Olly Moss (Ilustrador), Harry Potter é um garoto cujos pais, feiticeiros, foram assassinados por um poderosíssimo bruxo quando ele ainda era um bebê. Ele foi levado, então, para a casa dos tios que nada tinham a ver com o sobrenatural. Pelo contrário. Até os 10 anos, Harry foi uma espécie de gata borralheira: maltratado pelos tios, herdava roupas velhas do primo gorducho, tinha óculos remendados e era tratado como um estorvo.",
+        link: "https://rocco.com.br/especial/loja-harry-potter/",
+        tags: "filmes"
+    },
+    {
+        titulo: "A Biblioteca da Meia-Noite",
+        descricao: "A Biblioteca da Meia-Noite Capa comum – 27 setembro 2021, Edição Português  por Matt Haig (Autor), Adriana Fidalgo (Tradutor), Aos 35 anos, Nora Seed é uma mulher cheia de talentos e poucas conquistas. Arrependida das escolhas que fez no passado, ela vive se perguntando o que poderia ter acontecido caso tivesse vivido de maneira diferente. Após ser demitida e seu gato ser atropelado, Nora vê pouco sentido em sua existência e decide colocar um ponto final em tudo. Porém, quando se vê na Biblioteca da Meia-Noite, Nora ganha uma oportunidade única de viver todas as vidas que poderia ter vivido. Neste lugar entre a vida e a morte, e graças à ajuda de uma velha amiga, Nora pode, finalmente, se mudar para a Austrália, reatar relacionamentos antigos – ou começar outros –, ser uma estrela do rock, uma glaciologista, uma nadadora olímpica... enfim, as opções são infinitas. Mas será que alguma dessas outras vidas é realmente melhor do que a que ela já tem? Em A Biblioteca da Meia-Noite , Nora Seed se vê exatamente na situação pela qual todos gostaríamos de poder passar: voltar no tempo e desfazer algo de que nos arrependemos. Diante dessa possibilidade, Nora faz um mergulho interior viajando pelos livros da Biblioteca da Meia-Noite até entender o que é verdadeiramente importante na vida e o que faz, de fato, com que ela valha a pena ser vivida.",
+        link: "https://www.record.com.br/produto/a-biblioteca-da-meia-noite/",
+        tags: "filmes"
+    },
+    {
+        titulo: "O Príncipe",
+        descricao: "O Príncipe - Edição de Luxo Capa dura – 15 agosto 2023 por Nicolau Maquiavel (Autor), Editora Garnier (Editor), Fábio Kataoka (Tradutor), Publicado em 1532 pelo controverso e complexo filósofo Maquiavel, O Príncipe traz uma abordagem direta e desapaixonada acerca da natureza e do poder político. Neste tratado, o autor defende conceitos pragmáticos, entre os quais a ideia de um governante ser capaz de agir sem restrições morais para manter o controle de seu estado. Com base em exemplos históricos e em sua própria experiência como diplomata, Maquiavel também oferece conselhos práticos sobre como conquistar e manter o poder, reforçando, ainda, a importância de manter a lealdade do povo e dos aliados. Trata-se de um livro que oferece uma visão única e fascinante do mundo político de sua época, repleta de intrigas, conflitos e traições.",
+        link: "https://edipro.com.br/livro/o-principe/",
+        tags: "filmes"
+    },
+
+
+];
+
